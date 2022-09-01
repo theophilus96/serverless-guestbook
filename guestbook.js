5ad98f1c2f8a81b48004ad19248a8511b7d4a24a
Theo <50838737+theophilus96@users.noreply.github.com>
2022-09-01 18:00:45 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://fe77741f.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
